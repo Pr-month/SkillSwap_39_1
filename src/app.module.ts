8e164f080f519f4adf94cb14c8d6da3a681bf3af
an-sbl <an.sbl@yandex.ru>
2026-03-18 22:39:25 +0300

    feat: конфиги для основного модуля и БД PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,23 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config'; 
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { dbConfig, IDBConfig } from './config/db.config';
+import { appConfig } from './config/app.config';
 
 @Module({
-  imports: [
+ imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: '.env',
+      load: [appConfig, dbConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [dbConfig.KEY],
+      useFactory: (db: IDBConfig) => db,
     }),
     AuthModule,
     UsersModule],
