23fd5445deabbcce375d1603b926f5806024651f
webantonio-git <webantonio@yandex.ru>
2026-03-22 22:16:00 +0300

    feat(auth): register user

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,16 +2,26 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigType } from '@nestjs/config';
+import { jwtConfig } from 'src/config/jwt.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entities';
 
 @Module({
   imports: [
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '1h' },
+    ConfigModule.forFeature(jwtConfig),
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule.forFeature(jwtConfig)],
+      inject: [jwtConfig.KEY],
+      useFactory: (config: ConfigType<typeof jwtConfig>) => ({
+        secret: config.secret,
+        signOptions: { expiresIn: config.expiresIn },
+      }),
     }),
   ],
   controllers: [AuthController],
   providers: [AuthService],
-  exports: [JwtModule],
+  exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
