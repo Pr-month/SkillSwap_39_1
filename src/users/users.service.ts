8f927d1f0912bc4edbce01221fc8868c07f7c8b0
Mariia Sunnikova <honey.born@mail.ru>
2026-03-23 09:32:38 +0300

    feat(users): реализован эндпоинт GET users/me/:id

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../entities/user.entities';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
@@ -12,8 +24,19 @@ export class UsersService {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    if (isNaN(id) || id <= 0) {
+      throw new BadRequestException('Некорректный id');
+    }
+
+    const user = await this.usersRepository.findOneBy({ id: `${id}` });
+
+    if (!user) {
+      throw new NotFoundException('Пользователь не найден');
+    }
+
+    // return `This action returns a #${id} user`;
+    return user;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
