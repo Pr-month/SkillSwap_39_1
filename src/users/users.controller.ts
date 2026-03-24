4ee40c4e496acdc8d414ff0a28711b96257c339c
Roland <machodin@yandex.ru>
2026-03-24 17:03:16 +0300

    resolve conflicts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,3 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
-import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { Controller, Post, Body, Get, Param, UseGuards, Patch, Req } from "@nestjs/common";
 import { JwtAuthGuard } from "src/auth/guards/jwt-access.guard";
 import { AuthRequest } from "src/auth/types/types";
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,14 +1,4 @@
-import {
-  BadRequestException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-import { InjectRepository } from '@nestjs/typeorm';
-import { User } from '../entities/user.entities';
-import { Repository } from 'typeorm';
-import { Injectable, NotFoundException, BadRequestException } from "@nestjs/common";
+import { Injectable, BadRequestException, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { User } from "src/entities/user.entities";
 import { Repository } from "typeorm";
