d266b2f4f977d39da6071f46fae48f6a1a375469
Mariia Sunnikova <honey.born@mail.ru>
2026-03-25 15:31:41 +0300

    feat: пароль и рефреш токен не отправляются в запросах

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +12,8 @@ async function bootstrap() {
       forbidNonWhitelisted: true, // выбрасывает ошибку, если пришли лишние поля
     }),
   );
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   await app.listen(process.env.PORT ?? 3000);
 }
