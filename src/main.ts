ce777490f899c4f98eb0f5c771d1c929581a0d10
webantonio-git <webantonio@yandex.ru>
2026-03-25 13:35:07 +0300

    use port from app config

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,22 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { appConfig, IAppConfig } from './config/app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // Валидация входящих данных в контроллерах
+
+
   app.useGlobalPipes(
     new ValidationPipe({
-      transform: true, // автоматически преобразует входные данные в экземпляры классов
-      whitelist: true, // удаляет из объекта все свойства, которых нет в DTO
-      forbidNonWhitelisted: true, // выбрасывает ошибку, если пришли лишние поля
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  const config = app.get<IAppConfig>(appConfig.KEY);
+
+  await app.listen(config.port);
 }
 bootstrap();
