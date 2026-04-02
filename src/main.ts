82021e4b908a72ce9a2edc1230768cd9fb89945c
webantonio-git <webantonio@yandex.ru>
2026-04-02 22:42:36 +0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { join } from 'path';
 import { AppModule } from './app.module';
 import { appConfig, IAppConfig } from './config/app.config';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  // Валидация входящих данных в контроллерах
+
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalPipes(
     new ValidationPipe({
@@ -17,8 +18,16 @@ async function bootstrap() {
     }),
   );
 
-  // Раздача статических файлов из папки public
   app.useStaticAssets(join(process.cwd(), 'public'));
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('SkillSwap API')
+    .setDescription('Документация API проекта SkillSwap')
+    .setVersion('1.0')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, swaggerDocument);
 
   const config = app.get<IAppConfig>(appConfig.KEY);
   await app.listen(config.port);
