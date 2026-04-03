c7480fc0bbc307473acd63a4ff42412551cb548b
Mariia Sunnikova <honey.born@mail.ru>
2026-04-03 10:41:29 +0300

    feat(request): добавлен эндпоинт GET request/outgoing

--- src/requests/requests.controller.ts
+++ src/requests/requests.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common';
-import { RequestsService } from './requests.service';
+import {
+  Controller,
+  DefaultValuePipe,
+  Get,
+  ParseIntPipe,
+  Query,
+  Req,
+} from '@nestjs/common';
+import { RequestsService } from './entities/requests.service';
+import { AuthRequest } from '../auth/types/types';
 
 @Controller('requests')
 export class RequestsController {
   constructor(private readonly requestsService: RequestsService) {}
+
+  @Get('outgoing')
+  findOutgoing(
+    @Req() req: AuthRequest,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number, //для пагинации через query-параметры при необходимости
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+  ) {
+    return this.requestsService.findOutgoing(req.user.sub, page, limit);
+  }
 }
