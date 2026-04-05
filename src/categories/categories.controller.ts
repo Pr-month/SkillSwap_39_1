67004e47a5d389bd59fce856798a2b3d79c0d1b9
Kirill Dolgikh <w1tchan@yandex.ru>
2026-04-05 18:34:17 +0300

    feat: Role guards on patch

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -35,6 +35,8 @@ export class CategoriesController {
     return this.categoriesService.findOne(id);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles([Role.ADMIN])
   @Patch(':id')
   update(
     @Param('id') id: string,
