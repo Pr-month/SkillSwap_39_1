9b406755e7b23728041f3ed6c72d4332ef119eb0
webantonio-git <webantonio@yandex.ru>
2026-04-09 21:24:51 +0300

    DELETE cities

--- src/cities/cities.controller.ts
+++ src/cities/cities.controller.ts
@@ -6,15 +6,22 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { CitiesService } from './cities.service';
 import { CreateCityDto } from './dto/create-city.dto';
 import { UpdateCityDto } from './dto/update-city.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-access.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/users/decorators/roles.decorator';
+import { Role } from 'src/common/enums/role.enum';
 
 @Controller('cities')
 export class CitiesController {
   constructor(private readonly citiesService: CitiesService) {}
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles([Role.ADMIN])
   @Post()
   create(@Body() createCityDto: CreateCityDto) {
     return this.citiesService.create(createCityDto);
@@ -30,11 +37,15 @@ export class CitiesController {
     return this.citiesService.findOne(id);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles([Role.ADMIN])
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCityDto: UpdateCityDto) {
     return this.citiesService.update(id, updateCityDto);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles([Role.ADMIN])
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.citiesService.remove(id);
