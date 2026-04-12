0934751c52ece602569273f3a5769f0542d586af
an-sbl <an.sbl@yandex.ru>
2026-04-12 18:00:12 +0300

    feat: e2e тесты для auth сервиса

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Inject, Injectable, UnauthorizedException, ConflictException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { JwtService } from '@nestjs/jwt';
 import { Repository } from 'typeorm';
@@ -20,20 +20,29 @@ export class AuthService {
   ) {}
 
   async register(registerDto: RegisterDto) {
-    const hashedPassword = await this.hashData(registerDto.password);
+    try{
+      const hashedPassword = await this.hashData(registerDto.password);
 
-    const user = this.usersRepository.create({
-      ...registerDto,
-      birthdate: registerDto.birthdate ? new Date(registerDto.birthdate) : null,
-      password: hashedPassword,
-    });
+      const user = this.usersRepository.create({
+        ...registerDto,
+        birthdate: registerDto.birthdate ? new Date(registerDto.birthdate) : null,
+        password: hashedPassword,
+      });
 
-    const savedUser = await this.usersRepository.save(user);
+      const savedUser = await this.usersRepository.save(user);
 
-    const tokens = await this.generateTokens(savedUser.id, savedUser.email);
-    await this.updateRefreshToken(savedUser.id, tokens.refreshToken);
+      const tokens = await this.generateTokens(savedUser.id, savedUser.email);
+      await this.updateRefreshToken(savedUser.id, tokens.refreshToken);
 
-    return tokens;
+      return tokens;
+    }
+    catch (error) {
+        if (error.code === '23505') { 
+          throw new ConflictException('Пользователь с таким email уже существует');
+        }
+        throw error;
+      }
+
   }
 
   async login(loginDto: LoginDto) {
