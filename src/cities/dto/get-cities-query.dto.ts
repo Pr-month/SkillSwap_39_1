497610e33602c566e04da0aa63fb0072470d60f1
Maksim Zhovna <m.zhovna@1440.space>
2026-04-26 20:51:54 +0300

    docs(cities): добавил swagger для cities

--- src/cities/dto/get-cities-query.dto.ts
+++ src/cities/dto/get-cities-query.dto.ts
@@ -1,8 +1,14 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
 import { IsOptional, IsString, MaxLength } from 'class-validator';
 import { normalizeString } from '../../common/utils';
 
 export class GetCitiesQueryDto {
+  @ApiPropertyOptional({
+    description: 'Поиск городов по части названия',
+    example: 'моск',
+    maxLength: 150,
+  })
   @IsOptional()
   @IsString()
   @MaxLength(150)
