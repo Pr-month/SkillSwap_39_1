36d99813b30732f4034dbf81fff41147e52d4429
Kirill Dolgikh <w1tchan@yandex.ru>
2026-04-26 13:52:06 +0300

    fix: users swagger DTO

--- src/users/dto/get-users-query.dto.ts
+++ src/users/dto/get-users-query.dto.ts
@@ -1,17 +1,29 @@
 import { Type } from 'class-transformer';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsInt, IsOptional, Max, Min } from 'class-validator';
 
 export class GetUsersQueryDto {
+  @ApiPropertyOptional({
+    example: 1,
+    description: 'Номер страницы',
+    minimum: 1,
+  })
   @IsOptional()
   @IsInt()
   @Min(1)
   @Type(() => Number)
-  page: number;
+  page?: number;
 
+  @ApiPropertyOptional({
+    example: 10,
+    description: 'Количество элементов на странице, максимум 50',
+    minimum: 1,
+    maximum: 50,
+  })
   @IsOptional()
   @IsInt()
   @Min(1)
   @Max(50)
   @Type(() => Number)
-  limit: number;
+  limit?: number;
 }
